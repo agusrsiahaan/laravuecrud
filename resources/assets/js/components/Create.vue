e021491180b7190f14e8061f01309e748319052d
Agus R Siahaan <agusronaldo2@gmail.com>
2020-04-26 23:26:12 -0700

    make index & create page

---
+++ resources/assets/js/components/Create.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="container">
+                <div class="card card-default">
+                    <div class="card-header">Create New Post</div>
+
+                    <div class="card-body">
+                        <form>
+                            <div class="form-group">
+                                <input type="text" name="" placeholder="Title..." class="form-control">
+                            </div>
+                            <div class="form-group">
+                                <textarea rows="5" name="" placeholder="Description..." class="form-control"></textarea>
+                            </div>
+                            <router-link to="/" class="btn btn-warning">Cancel</router-link>
+                            <button class="btn btn-success">Create</button>
+                        </form>
+                    </div>
+                </div>
+        </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
---
+++ resources/assets/js/components/Index.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="card">
+        <div class="card-header">
+            <router-link to="/create" class="btn btn-info float-right">Create New Post</router-link>
+        </div>
+        <div class="card-body">
+            <table class="table table-bordered">
+                <thead>
+                <tr>
+                    <th>Title</th>
+                    <th>Description</th>
+                    <th width="110"></th>
+                    <th width="100"></th>
+                    <th width="120"></th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <td>John</td>
+                    <td>Doe</td>
+                    <td><button class="btn btn-info"><i class="fa fa-eye"></i> View</button></td>
+                    <td><button class="btn btn-warning"><i class="fa fa-pencil-square-o"></i> Edit</button></td>
+                    <td><button class="btn btn-danger"><i class="fa fa-trash"></i> Delete</button></td>
+                </tr>
+             
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
